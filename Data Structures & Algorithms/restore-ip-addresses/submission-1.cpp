27e9ee83bc3adbeafee0778205972d3e635e08eb
Shantanu-pradhan <78226343+Shantanu-pradhan@users.noreply.github.com>
2026-05-23 18:08:49 +0530

    Add: restore-ip-addresses - submission-1

---
+++ Data Structures & Algorithms/restore-ip-addresses/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string>res;
+        vector<string>substr;
+        if(s.length() > 12){
+            return res;
+        }
+        find_all_ip(s, res, substr, 0);
+        return res;
+    }
+    void find_all_ip(string &s, vector<string> &res, vector <string> &substr, int index){
+        if(substr.size() == 4){
+            if(index == s.size()){
+                res.push_back(substr[0]+'.'+substr[1]+'.'+substr[2]+'.'+substr[3]);
+                return;
+            }
+            return;
+        }
+        for(int i=1; i<=3; i++){
+            if(index+i > s.size()){
+                break;
+            }
+            if(stoi(s.substr(index, i)) > 255){
+                break;
+            }
+            string part = s.substr(index, i);
+            if(part.length() > 1 && part[0] == '0'){
+                break;
+            }
+            substr.push_back(part);
+            find_all_ip(s, res, substr, index+i);
+            substr.pop_back();
+        }
+        return;
+    }
+};
