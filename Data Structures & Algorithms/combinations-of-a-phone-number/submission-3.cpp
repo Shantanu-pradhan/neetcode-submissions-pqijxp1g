0369fabcb2e948e7ea78436b110ed0906617d215
Shantanu-pradhan <78226343+Shantanu-pradhan@users.noreply.github.com>
2026-05-24 10:18:48 +0530

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+vector<string>maping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> letterCombinations(string digits) {
+        vector<string>res;
+        if(digits.length() == 0){
+            return res;
+        }
+        string substr;
+        find_comb(digits, 0, substr, res);
+        return res;
+    }
+    void find_comb(string digits, int index, string &substr, vector<string>&res){
+        if(substr.length() == digits.length()){
+            res.push_back(substr);
+            return;
+        }
+        int num = digits[index] - '0';
+        string sstring = maping[num];
+        for(int i=0; i<sstring.length(); i++){
+            substr.push_back(sstring[i]);
+            find_comb(digits, index+1, substr, res);
+            substr.pop_back();
+        }
+        return;
+    }
+};
